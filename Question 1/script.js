42f00037184c303f8a9e95c5b7f79e503178e825
samuelckraft <samuelckraft@gmail.com>
2024-06-26 22:12:56 -0400

    update

---
+++ Question 1/script.js
@@ -0,0 +1,16 @@
+//Task 1 & 2
+
+let studentName = "Alice";
+let studentAge = 15;
+let studentGrade = 10;
+let isPresent = true;
+
+
+//Task 3
+console.log("Name: " + studentName);
+
+console.log("Age: " + studentAge);
+
+console.log("Grade: " + studentGrade);
+
+console.log("Present: " + isPresent);
---
+++ Question 2/script.js
@@ -0,0 +1,34 @@
+//Task 1 & 2
+
+let y = 5;
+
+let x = 10;
+
+
+//Task 3
+console.log("Sum:", x + y);
+console.log("Difference:", x - y);
+console.log("Product:", x * y);
+console.log("Quotient:", x / y);
+
+//Task 4
+
+x = 20
+
+y = 10
+
+console.log("Updated num1:", x);
+console.log("Updated num2:", y);
+
+//Task5
+
+console.log("Is num1 equal to num2?", x == y);
+console.log("Is num1 greater than num2?", x > y);
+console.log("Is num1 not equal to num2?", x != y);
+
+//Task 6
+
+let isPositive = x > 0 && y > 0;
+let isEven = x%2 == 0 || y%2 == 0;
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
